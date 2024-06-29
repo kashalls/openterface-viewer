3a116d093bf6ceba2751abbbdb550b8503380ec9
Jordan Jones <jordpjones@gmail.com>
2024-06-28 18:44:00 -0700

    chore: link points to external site, needs external directive.

--- src/components/Unsupported.vue
+++ src/components/Unsupported.vue
@@ -14,7 +14,11 @@ const { supported } = useBrowserSupport()
       <AlertDialogHeader>
         <AlertDialogTitle>Unsupported Browser</AlertDialogTitle>
         <AlertDialogDescription>
-          <NuxtLink to="https://developer.mozilla.org/en-US/docs/Web/API/Web_Serial_API#browser_compatibility" class="text-foreground underline">Only Chrome/Edge browsers fully support the WebSerial API.</NuxtLink><br><br> You will only be able to view your camera using this application. Please switch to a supported browser or use the <NuxtLink to="https://openterface.com" class="text-foreground underline">Official Openterface Application.</NuxtLink>
+          <NuxtLink external to="https://developer.mozilla.org/en-US/docs/Web/API/Web_Serial_API#browser_compatibility"
+            class="text-foreground underline">Only Chrome/Edge browsers fully support the WebSerial API.</NuxtLink>
+          <br><br> You will only be able to view your camera using this application. Please switch to a supported
+          browser or use the <NuxtLink to="https://openterface.com" class="text-foreground underline">Official
+            Openterface Application.</NuxtLink>
         </AlertDialogDescription>
       </AlertDialogHeader>
       <AlertDialogFooter>
