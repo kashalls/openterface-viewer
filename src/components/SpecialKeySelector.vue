f5f208eed2e3cc1d817309185cd6210a4381e3dc
Jordan Jones <jordpjones@gmail.com>
2024-07-17 04:01:37 -0700

    chore: misc cleaning

---
+++ src/components/SpecialKeySelector.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const selected = ref([])
+
+function getFunctionKeys() {
+    const fnKeys = [] as Array<object>;
+
+    for (let i = 1; i <= 12; i++) {
+        const keyLabel = `F${i}`;
+        const keyValue = WINDOWS_KEYMAP[keyLabel];
+
+        fnKeys.push({ label: keyLabel, value: keyValue });
+    }
+
+    return fnKeys;
+}
+
+const keys = computed(() => {
+    return [...getFunctionKeys()]
+})
+</script>
+
+<template>
+    <USelectMenu v-model="selected" :options="keys" multiple placeholder="Select keys" />
+</template>
