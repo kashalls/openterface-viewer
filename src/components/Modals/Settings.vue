6ef94c81fbc13fbcf834974462b2c611819ea569
Jordan Jones <jordpjones@gmail.com>
2024-07-17 04:01:24 -0700

    chore: migrate the unsupported modal to nuxt-ui

---
+++ src/components/Modals/Settings.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const modal = useModal()
+const mouseModes = ['Absolute', 'Relative']
+const mouseMode = ref('Absolute')
+</script>
+
+<template>
+    <UModal>
+        <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+            <template #header>
+                <h1 class="font-semibold text-lg">Settings</h1>
+            </template>
+
+            <div class="grid grid-rows-3 gap-2">
+                <div>
+                    <h2 class="font-medium">Display</h2>
+                    <p class="italic">Not supported yet...</p>
+                </div>
+                <div>
+                    <h2 class="font-medium">Mouse</h2>
+                    <div class="grid grid-cols-2">
+                        <UFormGroup label="Mouse Mode">
+                            <USelectMenu v-model="mouseMode" :options="mouseModes" />
+                        </UFormGroup>
+                    </div>
+                </div>
+                <div>
+                    <h2 class="font-medium">Keyboard</h2>
+                    <p class="italic">Not supported yet...</p>
+                </div>
+            </div>
+
+            <template #footer>
+                <div class="flex flex-row justify-between">
+                    <div class="justify-start">
+                        <UButton label="Reset" variant="ghost" color="red" />
+                    </div>
+                    <div class="justify-end">
+                        <UButton label="Save" @click="modal.close()" />
+                    </div>
+                </div>
+            </template>
+        </UCard>
+    </UModal>
+</template>
---
+++ src/components/Modals/UnsupportedBrowser.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+const modal = useModal()
+</script>
+
+<template>
+  <UModal :overlay="true">
+    <UCard>
+      <template #header>
+        <p class="font-semibold text-red-600">Unsupported Browser</p>
+      </template>
+      <div class="space-y-2">
+        <UButton color="primary" variant="link"
+          to="https://developer.mozilla.org/en-US/docs/Web/API/Web_Serial_API#browser_compatibility">
+          Only chrome & edge browsers fully support the WebSerial API (which this webapp requires).
+        </UButton>
+        <p class="textsm">You will only be able to view your camera using this application.</p>
+        <UButton color="primary" variant="link" to="https://openterface.com" class="text-foreground underline">Please
+          switch
+          to a supported
+          browser or use the
+          Official Openterface Application.</UButton>
+      </div>
+      <template #footer>
+        <UButton label="I Understand" color="rose" @click="modal.close()" class="ml-auto" />
+      </template>
+    </UCard>
+  </UModal>
+</template>
